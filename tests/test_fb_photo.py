89e8e3448ae9b73338b8ff68763ce9652e7aa09f
Raul Gutierrez Segales <rgs@itevenworks.net>
2013-06-17 23:24:25 -0700

    Cleanup tests to use unittest framework

--- tests/test_fb_photo.py
+++ tests/test_fb_photo.py
@@ -2,9 +2,11 @@
 
 from gi.repository import GObject
 
+import argparse
 import logging
 import time
 import sys
+import unittest
 
 sys.path.append("..")
 
@@ -12,124 +14,137 @@ from facebook.fb_photo import FbPhoto
 from facebook.fb_account import FbAccount
 
 
-def test_create_photo(loop):
-    def photo_created_cb(photo, photo_id, loop):
-        logging.debug("Photo created: %s" % (photo_id))
-        loop.quit()
+class TestFbPhoto(unittest.TestCase):
+    PER_TEST_TIMEOUT = 30000
+    photo_path = 'test.png'
 
-    photo = FbPhoto()
-    photo.connect('photo-created', photo_created_cb, loop)
-    photo.create(photo_path)
+    def setUp(self):
+        logging.debug("Starting (%d)", int(time.time()))
+        self._loop = GObject.MainLoop()
+        self._tid = GObject.timeout_add(self.PER_TEST_TIMEOUT, self._timeout_cb)
+        self._completed = False
 
-def test_add_comment(loop):
-    def photo_created_cb(photo, photo_id, loop):
-        logging.debug("Photo created: %s" % (photo_id))
+    def tearDown(self):
+        GObject.source_remove(self._tid)
 
-        def comment_added_cb(photo, comment_id, loop):
-            logging.debug("Comment created: %s" % (comment_id))
-            loop.quit()
+    def _finish_test(self):
+        self._completed = True
+        self._loop.quit()
+
+    def test_create_photo(self):
+        def photo_created_cb(photo, photo_id, callback):
+            logging.debug("Photo created: %s" % (photo_id))
+            callback()
             return False
 
-        photo = FbPhoto(photo_id)
-        photo.connect("comment-added", comment_added_cb, loop)
-        photo.add_comment("this is a test")
-        return False
+        photo = FbPhoto()
+        photo.connect('photo-created', photo_created_cb, self._finish_test)
+        photo.create(self.photo_path)
+        self._loop.run()
+        assert(self._completed)
 
-    photo = FbPhoto()
-    photo.connect('photo-created', photo_created_cb, loop)
-    photo.create(photo_path)
+    def test_add_comment(self):
+        def photo_created_cb(photo, photo_id, callback):
+            logging.debug("Photo created: %s" % (photo_id))
 
-def test_get_comments(loop):
-    def photo_created_cb(photo, photo_id, loop):
-        logging.debug("Photo created: %s" % (photo_id))
+            def comment_added_cb(photo, comment_id, callback):
+                logging.debug("Comment created: %s" % (comment_id))
+                callback()
+                return False
 
-        def comment_added_cb(photo, comment_id, loop):
-            logging.debug("Comment created: %s" % (comment_id))
-
-            def comments_downloaded_cb(photo, comments, loop):
-                logging.debug("%s comments for photo %s",
-                              len(comments), photo.fb_object_id)
-
-                for c in comments:
-                    logging.debug("Comment from %s with message: %s",
-                                  c["from"], c["message"])
-
-                loop.quit()
-
-            photo.connect('comments-downloaded',
-                          comments_downloaded_cb,
-                          loop)
-            photo.refresh_comments()
+            photo = FbPhoto(photo_id)
+            photo.connect("comment-added", comment_added_cb, callback)
+            photo.add_comment("this is a test")
             return False
 
-        photo = FbPhoto(photo_id)
-        photo.connect("comment-added", comment_added_cb, loop)
-        photo.add_comment("this is a test")
-        return False
+        photo = FbPhoto()
+        photo.connect('photo-created', photo_created_cb, self._finish_test)
+        photo.create(self.photo_path)
+        self._loop.run()
+        assert(self._completed)
 
-    photo = FbPhoto()
-    photo.connect('photo-created', photo_created_cb, loop)
-    photo.create(photo_path)
+    def test_get_comments(self):
+        def photo_created_cb(photo, photo_id, callback):
+            logging.debug("Photo created: %s" % (photo_id))
 
-def test_transfer_state_changed(loop):
-    states = []
-    states_started = []
-    states_completed = []
-    def transfer_state_changed_cb(photo, state, loop):
-        states.append(state)
+            def comment_added_cb(photo, comment_id, callback):
+                logging.debug("Comment created: %s" % (comment_id))
 
-        if 'started' in state:
-            states_started.append(state)
-        elif 'completed' in  state:
-            states_completed.append(state)
+                def comments_downloaded_cb(photo, comments, callback):
+                    logging.debug("%s comments for photo %s",
+                                  len(comments), photo.fb_object_id)
 
-        if len(states_started) == 1 and len(states_completed) == 1:
-            loop.quit()
+                    for c in comments:
+                        logging.debug("Comment from %s with message: %s",
+                                      c["from"], c["message"])
+                    callback()
+
+                photo.connect('comments-downloaded',
+                              comments_downloaded_cb,
+                              callback)
+                photo.refresh_comments()
+                return False
+
+            photo = FbPhoto(photo_id)
+            photo.connect("comment-added", comment_added_cb, callback)
+            photo.add_comment("this is a test")
+            return False
+
+        photo = FbPhoto()
+        photo.connect('photo-created', photo_created_cb, self._finish_test)
+        photo.create(self.photo_path)
+        self._loop.run()
+        assert(self._completed)
+
+    def test_transfer_state_changed(self):
+        states = []
+        states_started = []
+        states_completed = []
+        def transfer_state_changed_cb(photo, state, callback):
+            states.append(state)
+
+            if 'started' in state:
+                states_started.append(state)
+            elif 'completed' in  state:
+                states_completed.append(state)
+
+            if len(states_started) == 1 and len(states_completed) == 1:
+                callback()
+                return False
 
             logging.debug("transfer-state-changed events received:")
             for s in states:
-                logging.debug("- ", s)
+                logging.debug("- %s", s)
 
-    photo = FbPhoto()
-    photo.connect('transfer-state-changed',
-                  transfer_state_changed_cb, loop)
-    photo.create(photo_path)
+        photo = FbPhoto()
+        photo.connect('transfer-state-changed',
+                      transfer_state_changed_cb,
+                      self._finish_test)
+        photo.create(self.photo_path)
+        self._loop.run()
+        assert(self._completed)
 
+    def _timeout_cb(self):
+        self._loop.quit()
+        return False
 
-def timeout_cb(test_name, loop, failed):
-    failed.append(True)
-    loop.quit()
-    return False
+def _get_params():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--debug',
+                        type=bool,
+                        default=False)
+    parser.add_argument('access_token',
+                        help='token to run the tests with')
+    parser.add_argument('test_name',
+                        nargs='?',
+                        default='')
+    return parser.parse_args()
 
 if __name__ == '__main__':
-    tname = ''
-    len_args = len(sys.argv)
-    if len_args < 2:
-        logging.debug("Tests need an access_token!")
-        exit(1)
-    elif len_args == 3:
-        tname = sys.argv[2]
+    params = _get_params()
 
-    photo_path = "test.png"
-    access_token = sys.argv[1]
-    FbAccount.set_access_token(access_token)
+    if params.debug:
+        logging.basicConfig(level=logging.DEBUG)
 
-    if tname != '':
-        tests = [eval(tname)]
-    else:
-        tests = [eval(t) for t in dir() if t.startswith('test_')]
-
-    for t in tests:
-        print "\n* Starting %s (%s)" % (t.__name__, time.time())
-        loop = GObject.MainLoop()
-        failed = []
-        tid = GObject.timeout_add(30000, timeout_cb, t.__name__, loop, failed)
-        t(loop)
-        loop.run()
-        GObject.source_remove(tid)
-        if len(failed) == 0:
-            result = "\033[92mpassed\033[0m"
-        else:
-            result = "\033[91mfailed\033[0m"
-        print "* Finished %s, result = %s  (%s)\n" % (t.__name__, result, time.time())
-
+    FbAccount.set_access_token(params.access_token)
+    unittest.main(argv=['test_fb_photo'])
